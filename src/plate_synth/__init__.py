6da21a6c7d686b7a77b673b464a208cd35a18490
Maximilian Paul Kretschmer <m.kretschmer@campus.tu-berlin.de>
2026-09-17 17:11:34 +0200

    chore: expose finalized modal contract helpers

--- src/plate_synth/__init__.py
+++ src/plate_synth/__init__.py
@@ -1,15 +1,25 @@
 """Neural modal plate synthesizer core."""
 
-from .config import SynthParameters
+from .config import SynthParameters, SUPPORTED_BOUNDARY_CONDITIONS
 from .geometry import GeometryDescription, make_geometry
-from .modal_backend import AnalyticRectangleBackend, ModalBackend, ModalBasis, UnsupportedGeometryError
+from .modal_backend import (
+    AnalyticRectangleBackend,
+    ModalBackend,
+    ModalBasis,
+    UnsupportedBoundaryConditionError,
+    UnsupportedGeometryError,
+    physical_mass_weights,
+)
 
 __all__ = [
     "AnalyticRectangleBackend",
     "GeometryDescription",
     "ModalBackend",
     "ModalBasis",
+    "SUPPORTED_BOUNDARY_CONDITIONS",
     "SynthParameters",
+    "UnsupportedBoundaryConditionError",
     "UnsupportedGeometryError",
     "make_geometry",
+    "physical_mass_weights",
 ]
